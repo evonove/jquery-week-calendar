4937aeb884e3d0c0811904e94f7000a8e6c53b81
Emanuele Palazzetti <emanuele.palazzetti@evonove.it>
2013-06-21 17:42:58 +0200

    Added utils for date parsing

---
+++ src/utils.js
@@ -0,0 +1,68 @@
+'use strict';
+
+/**************
+* Modal utils *
+**************/
+
+/**
+ * Format date object to Pickadate.js valid date.
+ * @param {Object} date object to parse
+ * @return {Array} array with [year, month, day] values
+ */
+function parseDate(date) {
+	var momentDate = moment(date);
+	return [momentDate.year(), momentDate.month(), momentDate.date()];
+}
+
+/**
+ * Format date object to Pickadate.js valid time.
+ * @param {Object} date object to parse
+ * @return {Array} array with [hour, minutes] values
+ */
+function parseTime(date) {
+	var momentDate = moment(date);
+	return [momentDate.hour(), momentDate.minutes()];
+}
+
+/**
+ * Use Pickadate.js to recover formatted date.
+ * @param {Object} input field get as a selector
+ */
+function getDateValue(selector) {
+	return selector.pickadate('picker').get('select', 'yyyy-mm-dd');
+}
+
+/**
+ * Use Pickadate.js to recover formatted time.
+ * @param {Object} input field get as a selector
+ */
+function getTimeValue(selector) {
+	return selector.pickatime('picker').get('select', 'HH:i');
+}
+
+/**
+ * Use Pickadate.js to set formatted date.
+ * @param {Object} input field get as a selector
+ * @param {Object} datetime object to set
+ */
+function setDateValue(selector, value) {
+	return selector.pickadate('picker').set('select', parseDate(value));
+}
+
+/**
+ * Use Pickadate.js to set formatted time.
+ * @param {Object} input field get as a selector
+ * @param {Object} datetime object to set
+ */
+function setTimeValue(selector, value) {
+	return selector.pickatime('picker').set('select', parseTime(value));
+}
+
+/**
+ * Use Moment.js to parse date and time into ISO-8601 format.
+ * @param {Object} date with a format YYYY-MM-DD (2000-12-31)
+ * @param {Object} time with a format HH:MM (23:59)
+ */
+function datetimeISOFormat(date, time) {
+	return moment(date + ' ' + time, 'YYYY-MM-DD hh:mm').format();
+}
