45edff5de99982508a48c6d94189ab2dba5a1edc
Emanuele Palazzetti <emanuele.palazzetti@evonove.it>
2013-06-26 17:15:13 +0200

    Moved all functions into modal ojbect with attributes able to store input fields

--- src/modal.js
+++ src/modal.js
@@ -10,35 +10,61 @@
 	var calendar = TimelyUi.calendar,
 		utils = TimelyUi.utils,
 		modal = calendar.modal,
-		userSelect = $('.modal-body #user'),
-		timeInterval = 0,
+		timeInterval = 60 / calendar.options.timeslotsPerHour,
 		eventId = 1; // TODO: remove this because it should be known by AngularJS
+
+	/* Attach inputs and functions to bootstrap modal object */
+	$.extend(modal, {
+		userSelect: $('.modal #user'),
+		title: $('.modal-header #title'),
+		userId: $('.modal-body #user'),
+		body: $('.modal-body #description'),
+
+		clear: function() {
+			this.title.val('');
+			this.body.val('');
+			calendar.removeUnsavedEvents();
+			delete this.options.calEvent;
+			return this;
+		},
+
+		load: function() {
+			this.userId.val(this.options.calEvent.userId);
+			utils.setDateValue(('startDate'), this.options.calEvent.start);
+			utils.setTimeValue(('startTime'), this.options.calEvent.start);
+			utils.setDateValue(('endDate'), this.options.calEvent.end);
+			utils.setTimeValue(('endTime'), this.options.calEvent.end);
+			return this;
+		},
+
+		save: function() {
+			this.options.calEvent = {
+				id: eventId,
+				title: this.title.val(),
+				userId: parseInt(this.userId.val(), 10),
+				start: utils.datetimeISOFormat(utils.getDateValue('startDate'), utils.getTimeValue('startTime')),
+				end: utils.datetimeISOFormat(utils.getDateValue('endDate'), utils.getTimeValue('endTime')),
+				body: this.body.val()
+			};
+
+			calendar.updateEvent(this.options.calEvent);
+			eventId += 1; // TODO: remove this because it should be known by AngularJS
+			return this;
+		}
+	});
 
 	/* Load users dynamically */
 	$.each(calendar.options.users, function(index, user) {
-		userSelect.append('<option value="{0}">{1}</option>'.format(user.id, user.name));
+		modal.userSelect.append('<option value="{0}">{1}</option>'.format(user.id, user.name));
 	});
 
 	/* Set all date and time widgets */
-	timeInterval = 60 / calendar.options.timeslotsPerHour;
 	$('.modal-body .datepicker').pickadate();
 	$('.modal-body .timepicker').pickatime({ interval: timeInterval, format: 'H:i' });
 
 	/* Save current event */
 	$('.modal-footer #modalSave').click(function() {
-		var calEvent = {
-			id: eventId,
-			title: $('.modal-header #title').val(),
-			userId: parseInt($('.modal-body #user').val(), 10),
-			start: utils.datetimeISOFormat(utils.getDateValue('startDate'), utils.getTimeValue('startTime')),
-			end: utils.datetimeISOFormat(utils.getDateValue('endDate'), utils.getTimeValue('endTime')),
-			body: $('.modal-body #description').val()
-		};
-
-		calendar.updateEvent(calEvent);
-		eventId += 1; // TODO: remove this because it should be known by AngularJS
-
-		modal.hide();
+		modal.save().hide();
 	});
 
 	/* Twitter bootstrap events handler */
@@ -46,17 +72,15 @@
 		if (typeof modal.options.calEvent === 'undefined') {
 			return event.preventDefault();
 		} else {
-			// Set input values in modal form
-			$('.modal-body #user').val(modal.options.calEvent.userId);
-			utils.setDateValue(('startDate'), modal.options.calEvent.start);
-			utils.setTimeValue(('startTime'), modal.options.calEvent.start);
-			utils.setDateValue(('endDate'), modal.options.calEvent.end);
-			utils.setTimeValue(('endTime'), modal.options.calEvent.end);
+			modal.load();
 		}
 	});
 
+	modal.$element.on('shown', function(event) {
+		modal.title.focus();
+	});
+
 	modal.$element.on('hide', function(event) {
-		delete modal.options.calEvent;
-		calendar.removeUnsavedEvents();
+		modal.clear();
 	});
 })(jQuery);
