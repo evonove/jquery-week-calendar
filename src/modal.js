90a708f81e4ebf383506087755edf72e4b07bcbd
Emanuele Palazzetti <emanuele.palazzetti@evonove.it>
2013-06-26 12:03:43 +0200

    Modal widget can save event on calendar

--- src/modal.js
+++ src/modal.js
@@ -4,34 +4,53 @@
  * Modal manager which creates, updates and deletes selected event
  */
 (function($) {
-	/* Set all date and time widgets */
+	/* Create and register modal object */
+	TimelyUi.calendar.modal = $('#ajax-modal').modal({ show: false }).data('modal');
+
 	var calendar = TimelyUi.calendar,
 		utils = TimelyUi.utils,
+		modal = calendar.modal,
 		timeInterval = 0,
-		id = 0; // TODO: it should be known by AngularJS
+		eventId = 1; // TODO: remove this because it should be known by AngularJS
 
+	/* Set all date and time widgets */
 	timeInterval = 60 / calendar.options.timeslotsPerHour;
-	$('.datepicker').pickadate();
-	$('.timepicker').pickatime({ interval: timeInterval, format: 'H:i' });
+	$('.modal-body .datepicker').pickadate();
+	$('.modal-body .timepicker').pickatime({ interval: timeInterval, format: 'H:i' });
+
+	/* Save current event */
+	$('.modal-footer #modalSave').click(function() {
+		var calEvent = {
+			id: eventId,
+			title: $('.modal-header #title').val(),
+			userId: parseInt($('.modal-body #user').val(), 10),
+			start: utils.datetimeISOFormat(utils.getDateValue('startDate'), utils.getTimeValue('startTime')),
+			end: utils.datetimeISOFormat(utils.getDateValue('endDate'), utils.getTimeValue('endTime')),
+			body: $('.modal-body #description').val()
+		};
+
+		calendar.updateEvent(calEvent);
+		eventId += 1; // TODO: remove this because it should be known by AngularJS
+
+		modal.hide();
+	});
 
 	/* Twitter bootstrap events handler */
-	$('#ajax-modal').on('show', function(event) {
-		var self = $('#' + event.target.id).data('modal');
-		if (typeof self.options.calEvent === 'undefined') {
+	modal.$element.on('show', function(event) {
+		if (typeof modal.options.calEvent === 'undefined') {
 			return event.preventDefault();
 		} else {
-			// Set values to inputs
-			utils.setDateValue(('startDate'), self.options.calEvent.start);
-			utils.setTimeValue(('startTime'), self.options.calEvent.start);
-			utils.setDateValue(('endDate'), self.options.calEvent.end);
-			utils.setTimeValue(('endTime'), self.options.calEvent.end);
+			// Set input values in modal form
+			$('.modal-body #user').val(modal.options.calEvent.userId);
+			utils.setDateValue(('startDate'), modal.options.calEvent.start);
+			utils.setTimeValue(('startTime'), modal.options.calEvent.start);
+			utils.setDateValue(('endDate'), modal.options.calEvent.end);
+			utils.setTimeValue(('endTime'), modal.options.calEvent.end);
 		}
 	});
 
-	$('#ajax-modal').on('hide', function(event) {
-		var self = $('#' + event.target.id).data('modal');
-
-		delete self.options.calEvent;
+	modal.$element.on('hide', function(event) {
+		delete modal.options.calEvent;
 		calendar.removeUnsavedEvents();
 	});
 })(jQuery);
