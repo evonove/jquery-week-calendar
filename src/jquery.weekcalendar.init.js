08d3b87e4fb494060bb033c8a6ce8d644c77687e
Emanuele Palazzetti <info@emanuelepalazzetti.eu>
2013-09-25 11:00:59 +0200

    MomentJS localization. Related to issue #24

--- src/jquery.weekcalendar.init.js
+++ src/jquery.weekcalendar.init.js
@@ -89,6 +89,14 @@ TimelyUi.dispelVodooMagic  = function() {
 };
 
 TimelyUi.init = function(id, conf) {
+    // i18n localization
+    var _availableLanguage = ['en', 'it'],
+        _currentLanguage = (window.navigator.userLanguage || window.navigator.language).split('-')[0],
+        _selectedLanguage = _.contains(_availableLanguage, _currentLanguage) ? _currentLanguage : 'en';
+
+    moment.lang(_selectedLanguage);
+
+    // Calendar initialization
     TimelyUi.calendar = $('#' + id).weekCalendar(conf).data('ui-weekCalendar');
     TimelyUi.calendar.conf = conf;
     TimelyUi.calendar.id = id;
